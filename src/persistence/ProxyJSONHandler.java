e3b396f1e518da9f502d65be76d48740c9c7777a
39393 <39393@host.docker.internal>
2025-07-10 22:39:28 +0200

    Gestione della persistenza su file JSON

---
+++ src/persistence/ProxyJSONHandler.java
@@ -0,0 +1,26 @@
+package persistence;
+
+import model.Libro;
+import java.util.List;
+
+public class ProxyJSONHandler implements Salvataggio {
+    private JSONHandlerReal realHandler;
+
+    public ProxyJSONHandler() {
+        this.realHandler = new JSONHandlerReal();
+    }
+
+    @Override
+    public void salvaSuFile(String path, List<Libro> libri) throws Exception {
+        System.out.println("🔐 [Proxy] Salvataggio file: " + path);
+        realHandler.salvaSuFile(path, libri);
+    }
+
+    @Override
+    public List<Libro> caricaDaFile(String path) throws Exception {
+        System.out.println("📂 [Proxy] Caricamento file: " + path);
+        return realHandler.caricaDaFile(path);
+    }
+}
+
+
